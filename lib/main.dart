0e6b1511e145143b0a1918901cafb8ef612bfe04
YasiruShaminda <yasirushaminda01@gmail.com>
2024-02-07 01:11:46 +0530

    Added Splash Screen and App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:sign_sync/routes/navigation.dart';
 import 'package:sign_sync/screens/HomeScreen.dart';
 
 Future<void> main() async {
+  FlutterNativeSplash.removeAfter(initialization);
   runApp(MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed((Duration(seconds: 1)));
 }
 
 class MyApp extends StatelessWidget {
