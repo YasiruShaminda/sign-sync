046d2bc23922075c3259e86cfa557856858944d9
YasiruShaminda <yasirushaminda01@gmail.com>
2024-02-07 00:20:45 +0530

    Improved camera screen UI.

--- lib/screens/TranslatorScreen.dart
+++ lib/screens/TranslatorScreen.dart
@@ -9,7 +9,6 @@ class TranslatorScreen extends StatefulWidget {
 }
 
 class _TranslatorScreenState extends State<TranslatorScreen> {
-
   List<CameraDescription>? cameras; //list out the camera available
   CameraController? controller; //controller for camera
   // XFile? image; //for caputred image
@@ -22,7 +21,7 @@ class _TranslatorScreenState extends State<TranslatorScreen> {
 
   loadCamera() async {
     cameras = await availableCameras();
-    if(cameras != null){
+    if (cameras != null) {
       controller = CameraController(cameras![0], ResolutionPreset.max);
       //cameras[0] = first camera, change to 1 to another camera
 
@@ -32,7 +31,7 @@ class _TranslatorScreenState extends State<TranslatorScreen> {
         }
         setState(() {});
       });
-    }else{
+    } else {
       print("NO any camera found");
     }
   }
@@ -40,23 +39,69 @@ class _TranslatorScreenState extends State<TranslatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-          children: [
-            Container(
-              height: 600,
-              width: 400,
-              child: controller == null?
-              const Center(
-                child: Text("Loading Camera..."),
-              ):
-              !controller!.value.isInitialized?
-              const Center(
-                child: CircularProgressIndicator(),
-              ):
-              CameraPreview(controller!),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            height: 500.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
             ),
-          ],
-        ),
-      );
+            child: controller == null
+                ? const Center(
+                    child: Text("Loading Camera..."),
+                  )
+                : !controller!.value.isInitialized
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : CameraPreview(controller!),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            height: 130.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Center(
+                child: Text(
+                    '- Translated Output Goes Here -',
+                  style: TextStyle(color: Colors.grey),
+                )
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
